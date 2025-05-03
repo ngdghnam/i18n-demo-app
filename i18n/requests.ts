92c47b1ba121a9b01cf7094db39499d09229670e
ngdghnam <stghoainam4002@gmail.com>
2025-05-03 20:47:22 +0700

    Update code

--- i18n/requests.ts
+++ i18n/requests.ts
@@ -1,6 +1,7 @@
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
 
+// Hi
 export default getRequestConfig(async () => {
   const cookieLocale = (await cookies()).get("MYNEXTAPP_LOCALE")?.value || "en";
   const locale = cookieLocale;
